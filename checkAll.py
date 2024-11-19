d6af5595f3087d5fedafbefdf4cf0b5b4c9a46eb
Cobble <cobblestonerules04@gmail.com>
2024-11-19 16:30:29 -0600

    Update checkAll.py
    updated the checkValID method to return the correct bool statement and finished the checkMaj method

--- checkAll.py
+++ checkAll.py
@@ -1,8 +1,12 @@
 def checkValID(inp):
-    if(len(inp) == 6 and inp[0,2] == "700"):
+    if(len(inp) == 6 and inp[:3] == "700"):
         return True
     else:
         return False
 def checkMaj(inp):
     Majors = ["CS", "CYBR", "SE"]
+    if(inp in Majors):
+        return True
+    else:
+        return False
 
