0b0860eac466f394a57126afe41acc6b70596147
noahm06304 <117481568+noahm06304@users.noreply.github.com>
2024-11-21 11:20:13 -0600

    DeleteMenu.py
    Adds comments, and made a few minor tweaks I forgot to delete while testing

--- DeleteStudent.py
+++ DeleteStudent.py
@@ -1,3 +1,4 @@
+#Menu that handles deleting students
 from JsonStuff import *
 import json
 
@@ -5,26 +6,27 @@ def deleteMenu():
 
     #Prompt the user to enter a student ID to delete
     IDInput = input("Please enter a Student ID to delete: ")
+
+    #Checks to make sure that the ID exists
+    #ID Exists
     if IDExists(IDInput) == True:
+        #Display student information
         print("=====Student Record=====")
         displayStudent(IDInput)
 
-        #Iterate the verification question until the user responds with Y or N
-        validResponse = False
-        while(validResponse == False):
-            response = input("Are you sure you want to delete this student from the record? Y or N: ")
+        #Verify that the user wants to delete the student
+        response = input("Are you sure you want to delete this student from the record? Y or N: ")
 
-            #Yes
-            if response.lower() == "y":
-                deleteStudent(IDInput)
-                print(f"Student {IDInput} has been deleted")
-            #No
-            elif response.lower() == "n":
-                print(f"Student {IDInput} has not been deleted")
-            #Invalid responses
-            else:
-                print("INVALID RESPONSE")
+        #Yes
+        if response.lower() == "y":
+            deleteStudent(IDInput)
+            print(f"Student {IDInput} has been deleted")
+        #No
+        elif response.lower() == "n":
+            print(f"Student {IDInput} has not been deleted")
+        #Invalid responses
+        else:
+            print("INVALID RESPONSE")
+    #ID does not exist
     else:
         print("Student ID does not exist")
-
-deleteMenu()
