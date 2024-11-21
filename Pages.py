931a7492ca1a544fb4cfe5c9f81fd57ee0a004f2
NateHochstatter <nrh20080@ucmo.edu>
2024-11-21 11:44:44 -0600

    Currently, all code in the file have proper commenting

--- Pages.py
+++ Pages.py
@@ -4,17 +4,20 @@ from checkAll import *
 
 # A basic function that prints the requested page
 def printPage(filename):
-    inputFile = open(filename, 'r')
-    print(inputFile.read())
-    inputFile.close()
+    inputFile = open(filename, 'r') #Get the file text
+    print(inputFile.read()) #Print the file text
+    inputFile.close() #Close the connection
 
+#Function for the start screen page
 def startPage():
-    printPage("Welcome.txt")
-    choice = input(str("Please Enter the Operation Code: "))
 
+    #If statement and while loop to keep the program running until they select the leave option and confirm
     ex = False
-    if (ex != True):
+    while (ex != True):
+        printPage("Welcome.txt")  # print the welcome text
+        choice = input(str("Please Enter the Operation Code: "))  # get the users choice
         while (choice != "6"):
+            #if statements for each option and the function correlating to them
             if (choice == "1"):
                 addStudentPage()
             elif (choice == "2"):
@@ -26,18 +29,21 @@ def startPage():
             elif (choice == "5"):
                 displayAllPage()
             else:
-                print("Wrong input enter a valid number")
-            printPage("Welcome.txt")
+                print("Wrong input enter a valid number") #error message
+            printPage("Welcome.txt") #Once the chosen function is done start the process again
             choice = input(str("Please Enter the Operation Code: "))
         leave = input(str("Do you want to Exit the System? Enter Y to Confirm: "))
-        if (leave == "Y"):
+        if (leave.upper() == "Y"):
             ex = True
 
-#Incomplete
+#Function for adding a student to the json
 def addStudentPage():
-    printPage("AddStudent.txt")
-    finalcheck = True
-    check = True
+    printPage("AddStudent.txt") #Print the addStudent text
+    finalcheck = True #Check for if the item already exists so it just stops and goes back to the start page
+    check = True #check so it goes through the while loop until a valid id is given
+
+    #the following series of while loops get a part of the students info only stopping if they get a valid input
+    #or if the info already exists
     while check and finalcheck:
         Id = str(input("Please enter the student ID: "))
         if checkValID(Id):
@@ -82,39 +88,43 @@ def addStudentPage():
             print(f"\u274c Invalid Student Major")
 
 
-    if finalcheck:
+    if finalcheck: #if none of the info already exists then complete the task
+        #Format the data collected before sending it to the function
         newStudent = {
             "ID": Id,
             "Name": Name,
             "Phone": Phone,
             "Major": Major}
 
+        # put the data into the function and print that the program was a success
         addStudent(newStudent)
         print("\u2714 New student record has been added")
-    #startPage()
 
 # The function for if option 5 is selected to display all students
 def displayAllPage():
+    #Simple function with me just printing the record text before calling the display function
     printPage("StudentRecord.txt")
     displayAll()
-    #startPage()
 
 #A function for choice 4 to go through the steps of getting the student id and then displaying that student
 def displayStudentPage():
-    Id = str(input("Please enter the student ID: "))
-    printPage("StudentRecord.txt")
-    if checkValID(Id):
+    Id = str(input("Please enter the student ID: ")) #Get the id
+    printPage("StudentRecord.txt") #print the record text
+    if checkValID(Id): #if statements to ensure the id is both valid and existing
         if IDExists(Id):
-            displayStudent(Id)
+            displayStudent(Id) #call the displayStudent function with the given id
         else:
-            print(f"\u274c The student Id {Id} does not exist")
+            print(f"\u274c The student Id {Id} does not exist") #error messages for if it is invalid or nonexistent
     else:
         print(f"\u274c The student Id {Id} is not valid")
-    #startPage()
 
+#a function for choice 3 for selecting a student and then modifying their data
 def modifyStudentPage():
+    #starts off by basically doing the displayStudent function but with a key change of setting a
+    # check to false if there is an issue
     Id = str(input("Please enter the student ID: "))
-    greatCheck = True
+    greatCheck = True #check for if there is an issue with the id which means it should stop
+    # trying to modify something
     printPage("StudentRecord.txt")
     if checkValID(Id):
         if IDExists(Id):
@@ -122,17 +132,17 @@ def modifyStudentPage():
         else:
             print(f"\u274c The student Id {Id} does not exist")
             greatCheck = False
-            #startPage()
     else:
         print(f"\u274c The student Id {Id} is not valid")
         greatCheck = False
-        #startPage()
 
-    if greatCheck:
+    if greatCheck: #if there were no issues continue as normal
+
+        #A series of while loops that until a valid new data is given
         check = True
         while check:
             newName = str(input("New Name: "))
-            if newName == "" or checkName(newName):
+            if newName == "" or checkName(newName): #It is ok if the data is left empty or is just a valid name
                 check = False
             else:
                 print(f"\u274c Invalid Student Name")
@@ -153,8 +163,10 @@ def modifyStudentPage():
             else:
                 print(f"\u274c Invalid Student Major")
 
-        if newName == "" and newPhone == "" and newMajor == "":
+        if newName == "" and newPhone == "" and newMajor == "": #If they left all the inputs empty
+            # send an error message
             print("\u274c Record not modified")
         else:
+            #otherwise call the function and say things went well
             modifyStudent(Id, newName, newPhone, newMajor.upper())
             print("\u2714 Student record updated successfully")
